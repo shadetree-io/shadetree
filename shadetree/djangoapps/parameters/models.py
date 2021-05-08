cf6e556f9839aaaeddceca7950acfa14b868c564
Lawrence McDaniel <lpm0073@gmail.com>
2021-05-08 15:28:00 -0500

    scaffold models

--- shadetree/djangoapps/parameters/models.py
+++ shadetree/djangoapps/parameters/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from model_utils.models import StatusModel, TimeStampedModel
+from model_utils import Choices
 
-# Create your models here.
+class HierarchialPublicationBaseClass(StatusModel, TimeStampedModel):
+    STATUS = Choices('draft', 'review', 'published')
+    name = models.CharField(max_length=50)
+    parent = models.ForeignKey("self", null=True, on_delete=models.CASCADE)
+
+
+class Project(HierarchialPublicationBaseClass):
+    #slug = models.SlugField(max_length = 200)
+    url = models.URLField(max_length=200)
+
+"""
+Package Classes
+"""
+class Package(HierarchialPublicationBaseClass):
+    """Base case for software packages: Application, Service, Framework, Project
+    """
+    parent_project = models.ForeignKey(Project, null=True, on_delete=models.CASCADE)
+
+    # fix me
+    package_type = models.CharField(max_length=50)
+
+
+"""
+Module Classes
+"""
+class Module(HierarchialPublicationBaseClass):
+    parent_package = models.ForeignKey(Package, null=True, on_delete=models.CASCADE)
+
+
+"""
+Parameter Classes
+"""
+class Parameter(HierarchialPublicationBaseClass):
+    parent_module = models.ForeignKey(Module, null=True, on_delete=models.CASCADE)
